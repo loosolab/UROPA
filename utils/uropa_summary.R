89509d7f4d40e16f4f6eb64f5f80c67e93f7c78b
Annika Fust <annika.fust@mpi-bn.mpg.de>
2018-01-16 13:43:10 +0100

    summary for invalid annotation

--- utils/uropa_summary.R
+++ utils/uropa_summary.R
@@ -135,6 +135,9 @@ features <- c()
 	df.uropa.final[,"distance"] <- as.numeric(df.uropa.final[,"distance"])
 	df.uropa.final <- df.uropa.final[complete.cases(df.uropa.final),]
 	if(nrow(df.uropa.final)==0){
+		mtext("UROPA summary", side=3, line=0,outer=FALSE, cex=2)
+		mtext("No valid peak annotations with specified query/queries, summary unfeasible!")
+		invisible(dev.off())
 		stop("No valid peak annotations with specified query/queries, summary unfeasible!")
 	}
 	anno.peaks <- nrow(df.uropa.final)
