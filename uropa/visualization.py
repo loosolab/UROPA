e59adc3b48920b1d207d43e150d6c2cf1123ccce
Hendrik Schultheis <Hendrik.Schultheis@mpi-bn.mpg.de>
2022-09-15 10:42:48 +0200

    added more documentation

--- uropa/visualization.py
+++ uropa/visualization.py
@@ -3,62 +3,127 @@
 # -------------------- plot functions -------------------- #
 
 
-def distribution(table):
+def distribution_plot(table, var, kind):
     """
-    #TODO add description here
+    Plot distribution of the selected numerical variable.
+    Distribution can be shown as boxplot, violinplot or histogram/kde.
 
     Parameters
     ----------
     table : pd.DataFrame
         Pandas dataframe containing the data.
+    var : <datatype>, <default value>
+        <param description>
+    kind : <datatype>, <default value>
+        <param description>
+    TODO add more parameters
 
     Returns
     -------
+    <datatype> :
+        <return description>
+    TODO should return the plotting object
     """
     pass
 
 
-def peak_count(table):
+def count_plot(table, var, kind):
     """
-    #TODO add description here
+    Count and plot the occurence of the selected categorical variable.
+    Either shown as a pie chart or bar plot.
 
     Parameters
     ----------
     table : pd.DataFrame
         Pandas dataframe containing the data.
+    var : <datatype>, <default value>
+        <param description>
+    kind : <datatype>, <default value>
+        <param description>
+    TODO add more parameters
 
     Returns
     -------
+    <datatype> :
+        <return description>
+    TODO should return the plotting object
     """
     pass
 
 
-def upset(table):
+def peak_count_plot(table, var, kind):
     """
-    #TODO add description here
+    Count and plot occurence of selected variable by peak.
 
     Parameters
     ----------
     table : pd.DataFrame
         Pandas dataframe containing the data.
+    var : <datatype>, <default value>
+        <param description>
+    kind : <datatype>, <default value>
+        <param description>
+    TODO add more parameters
 
     Returns
     -------
+    <datatype> :
+        <return description>
+    TODO should return the plotting object
     """
     pass
 
 
-def coverage(table):
+def upset_plot(table, var):
     """
-    #TODO add description here
+    Visualize overlaps in a set with an upset plot.
+    Similar to a Venn diagram but more readable especially with higher category count.
+
+    For more information see: https://ieeexplore.ieee.org/document/6876017
+
+    https://upsetplot.readthedocs.io/en/stable/
 
     Parameters
     ----------
     table : pd.DataFrame
         Pandas dataframe containing the data.
+    var : <datatype>, <default value>
+        <param description>
+    TODO add more parameters
 
     Returns
     -------
+    <datatype> :
+        <return description>
+    TODO should return the plotting object
+    """
+    pass
+
+
+def plot_grid(table, groupby, func, ncol=3, **kwargs):
+    """
+    Create a grid of plots.
+    Data is split by selected variable then forwarded to the plotting function.
+
+    Parameters
+    ----------
+    table : pd.DataFrame
+        Pandas dataframe containing the data.
+    groupby : str
+        Name of a column in `table` the data should be split by.
+    func : function
+        Plotting function with which the grid is populated.
+    ncol : int, default 3
+        Number of plots per row.
+    kwargs :
+        Additional parameters forwarded to `func`.
+
+    Returns
+    -------
+    matplotlib.figure.Figure :
+        Figure
+    matplotlib.axes.Axes or array of Axes :
+        Each ax contains a plot of the grid.
     """
     pass
 
@@ -90,12 +155,21 @@ def summary(allhits, finalhits, config, call, output):
     
     # ----- title page ----- #
     # contains number of annotated peaks
+    # cmd call
     # query overview
 
     # ----- plot pages ----- #
     # ---------------------- #
-    # distribution
-    # ---------------------- #
+    # distribution plot(s)
 
-    # ----- save pdf ----- #
+    # ---------------------- #
+    # count plot(s)
+    
+    # ---------------------- #
+    # peak count plot(s)
+    
+    # ---------------------- #
+    # upset plot(s)
+
+    # ------ save pdf ------ #
     pass
