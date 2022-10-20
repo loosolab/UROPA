f526b3869482a6dae44d0e8463f544d9515a4d22
kmlr81 <74097059+kmlr81@users.noreply.github.com>
2022-10-20 10:29:03 +0200

    Update visualization.py
    Change request implemented.

--- uropa/visualization.py
+++ uropa/visualization.py
@@ -172,10 +172,10 @@ def upset_plot(table, var="feature", peak_columns=["peak_chr", "peak_start", "pe
     table[var] = table[var].astype(str)
     
     # Creat new data frame that groupes by peaks and lists values for selected colum per peak
-    grouped_table = table.groupby(peak_columns, as_index=False)[var].apply(' '.join)
+    grouped_table = table.groupby(peak_columns, as_index=False)[var].apply(','.join)
     
     # Create new data frame in correct format as plotting input
-    plot_table = up.from_memberships(grouped_table[var].str.split(' '), data=grouped_table)
+    plot_table = up.from_memberships(grouped_table[var].str.split(','), data=grouped_table)
     
     fig = plt.figure(dpi=dpi)
     
